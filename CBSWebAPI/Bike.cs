8553d082e4461b4f2f021b78362e492072e9cb3c
David <git@davidmannes.de>
2021-08-27 16:19:44 +0200

    Add dummy bike controller

---
+++ CBSWebAPI/Bike.cs
@@ -0,0 +1,8 @@
+namespace CBSWebAPI
+{
+    public class Bike
+    {
+	    public int Id { get; set; }
+	    public string Name { get; set; }
+    }
+}
---
+++ CBSWebAPI/Controllers/BikeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CBSWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BikeController : ControllerBase
+    {
+	    private readonly ILogger<BikeController> _logger;
+
+        public BikeController(ILogger<BikeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Bike> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, rng.Next(1, 6)).Select(index => new Bike
+            {
+	            Id = index,
+	            Name = $"Fahrrad Nr. {index}"
+            })
+            .ToArray();
+        }
+    }
+}
